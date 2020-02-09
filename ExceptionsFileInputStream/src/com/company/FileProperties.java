68f256872179e0cbbab6b711172c5b58aa033337
1goresha <i.deryabin2017@mail.ru>
2020-02-09 13:51:23 +0300

    little changes

--- ExceptionsFileInputStream/src/com/company/FileProperties.java
+++ ExceptionsFileInputStream/src/com/company/FileProperties.java
@@ -4,9 +4,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class FileProperties {
-    public String getAbsolute(String fileName) throws FileNotFoundException {
-        FileUtil fileUtil = new FileUtil();
-        File file = fileUtil.openFile(fileName);
-        return file.getAbsolutePath();
+    public String getAbsolute(String fileName) throws FileNotFoundException{
+//        try{
+//            FileUtil fileUtil = new FileUtil();
+//            File file = fileUtil.openFile(fileName);
+//            return file.getAbsolutePath();
+//        }catch(FileNotFoundException e){
+//            throw new IllegalArgumentException(e);
+//        }
+
+            FileUtil fileUtil = new FileUtil();
+            File file = fileUtil.openFile(fileName);
+            return file.getAbsolutePath();
     }
 }
