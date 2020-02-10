258ccbe57156764ede90cb8c3c4e713c3e6b3ef0
1goresha <i.deryabin2017@mail.ru>
2020-02-10 08:32:44 +0300

    new experimnents with FileInputStream

---
+++ ExperimentsWithFileInputStream/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        FileUtil fileUtil = new FileUtil("input_1.txt");
+        fileUtil.readStreamFromFile();
+    }
+}
