fd5f3d8ea57396f252a7370a3086abda5a329436
1goresha <i.deryabin2017@mail.ru>
2020-02-23 10:19:12 +0300

    add LinkedList and interface IntegerList

---
+++ ArrayIntegerList/src/com/company/IntegerList.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public interface IntegerList {
+    void add(int element);
+    void addToBegin(int element);
+    int get(int index);
+    void insert(int element, int index);
+    void reverse();
+    int getSize();
+    void sort();
+    boolean contains(int element);
+    int indexOf(int element);
+}
---
+++ ArrayIntegerList/src/com/company/Node.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Node {
+    private int value;
+    private Node next;
+
+    public Node(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return value == node.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
