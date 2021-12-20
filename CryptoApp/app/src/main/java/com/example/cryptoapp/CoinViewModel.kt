17f1d25b80a59eb292e92e393d8b9095283c7308
1goresha <i.deryabin2017@mail.ru>
2021-12-20 23:07:34 +0300

    unfinished project part2

--- CryptoApp/app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
+++ CryptoApp/app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.example.cryptoapp.api.ApiFactory
 import com.example.cryptoapp.database.AppDatabase
-import io.reactivex.android.schedulers.AndroidSchedulers
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import com.example.cryptoapp.pojo.CoinPriceInfoRawData
+import com.google.gson.Gson
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
@@ -18,15 +20,43 @@ class CoinViewModel(application: Application): AndroidViewModel(application) {
 
     fun loadData(){
         val disposable = ApiFactory.apiFactory.getTopCoinInfo(limit = 50)
+            .map{
+                it.data.map { it.coinInfo?.name }.joinToString(",")
+            }
+            .flatMap {
+                ApiFactory.apiFactory.getFullPriceInfoList(fSyms = it)
+            }
+            .map {
+                getPriceListFromRawData(it)
+            }
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+//            .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                val coinsString = it.data.map { it.coinInfo?.name }.joinToString(",")
-                Log.d("LOAD_DATA", coinsString)
+                Log.d("LOAD_DATA", it.toString())
+                db.coinPriceInfoDao().insertPriceList(it)
             },{
                 Log.d("LOAD_DATA_ERROR", it.message.toString())
             })
         compositeDisposable.add(disposable)
+    }
+
+    private fun getPriceListFromRawData(coinPriceInfoRawData: CoinPriceInfoRawData): List<CoinPriceInfo>{
+        val result = arrayListOf<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if (jsonObject == null) return result
+        val coinKeys = jsonObject.keySet()
+        for (coinKey in coinKeys){
+            val currencyObject = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeys = currencyObject.keySet()
+            for (currencyKey in currencyKeys){
+                val coinPriceInfo = Gson().fromJson(
+                    currencyObject.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(coinPriceInfo)
+            }
+        }
+        return result
     }
 
     override fun onCleared() {
